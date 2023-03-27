f9509c554177df93bdb3169f04a25fa6aa340431
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-03-26 18:31:45 -0700

    typescript and next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NextUIProvider, createTheme } from '@nextui-org/react'
+
+const darkTheme = createTheme({type: 'dark'})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={darkTheme}>
+  <Component {...pageProps} />
+  </NextUIProvider>
+  )
 }
